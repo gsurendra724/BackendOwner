128754da637250033b2ff04f00fb544240375b11
Ishaan Godara <gsurendra724@gmail.com>
2018-12-15 09:33:25 +0530

    1
    1

---
+++ BackendOwner/BackendOwner/src/main/java/com/todtransports/www/BackendOwner/BackendOwnerApplication.java
@@ -0,0 +1,14 @@
+package com.todtransports.www.BackendOwner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOwnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOwnerApplication.class, args);
+	}
+
+}
+
---
+++ BackendOwner/BackendOwner/src/test/java/com/todtransports/www/BackendOwner/BackendOwnerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.todtransports.www.BackendOwner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendOwnerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
